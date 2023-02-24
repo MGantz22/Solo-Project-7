c36d8318e0e4ba6c104ca5473bdf5014d6094108
Mitchell Gantz <mgantz22@icloud.com>
2023-02-24 15:07:50 -0800

    passing pastry test 7 and 8

--- GantzBakery.Tests/ModelTests/PastryTests.cs
+++ GantzBakery.Tests/ModelTests/PastryTests.cs
@@ -19,6 +19,36 @@ namespace GantzBakery.Tests
       int orderPastry = 2;
       Assert.AreEqual(4, Pastry.PastryTotal(orderPastry));
     }
+    [TestMethod]
+    public void PastryClass_MathFourthFree3_Pastry()
+    {
+      int orderPastry = 3;
+      Assert.AreEqual(6, Pastry.PastryTotal(orderPastry));
+    }
+    [TestMethod]
+    public void PastryClass_MathFourthFree4_Pastry()
+    {
+      int orderPastry = 5;
+      Assert.AreEqual(8, Pastry.PastryTotal(orderPastry));
+    }
+    [TestMethod]
+    public void PastryClass_MathFourthFree5_Pastry()
+    {
+      int orderPastry = 6;
+      Assert.AreEqual(10, Pastry.PastryTotal(orderPastry));
+    }
+    [TestMethod]
+    public void PastryClass_MathFourthFree6_Pastry()
+    {
+      int orderPastry = 7;
+      Assert.AreEqual(12, Pastry.PastryTotal(orderPastry));
+    }
+    [TestMethod]
+    public void PastryClass_MathFourthFree7_Pastry()
+    {
+      int orderPastry = 8;
+      Assert.AreEqual(12, Pastry.PastryTotal(orderPastry));
+    }
 
   }
 }
