a53c0707daf6881bc29423aef03a31b85f95babf
Mitchell Gantz <mgantz22@icloud.com>
2023-02-24 14:16:48 -0800

    pass PastryClass  test 2

--- GantzBakery.Tests/ModelTests/PastryTests.cs
+++ GantzBakery.Tests/ModelTests/PastryTests.cs
@@ -13,6 +13,12 @@ namespace GantzBakery.Tests
       int orderPastry = 1;
       Assert.AreEqual(2, Pastry.PastryTotal(orderPastry));
     }
+    [TestMethod]
+    public void PastryClass_MathFourthFree2_Pastry()
+    {
+      int orderPastry = 2;
+      Assert.AreEqual(4, Pastry.PastryTotal(orderPastry));
+    }
 
   }
 }
